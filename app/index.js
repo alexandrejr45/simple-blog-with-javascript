b84c0235090bf59c1609a9f00f4abe5b8ff1a2f8
Alexandre Silva <alexandresilva933@gmail.com>
2020-05-22 18:09:25 -0300

    chore: Adição da página de cadastro

--- app/index.js
+++ app/index.js
@@ -1,6 +1,8 @@
+import './css/normalize.css'
 import './css/style.css';
+import './css/formulario.css'
+
 import { verifyFirebaseConfiguration, firebaseConnect} from './js/firebase-config';
 
 firebaseConnect();
 verifyFirebaseConfiguration();
-
